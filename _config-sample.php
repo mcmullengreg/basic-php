829603a8ebc6d8fe64b2bb034a529f569b649252
Greg McMullen <mcmullen.greg@gmail.com>
2019-11-14 21:31:36 -0500

    config sample

---
+++ _config-sample.php
@@ -0,0 +1,10 @@
+<?php
+  
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  
+  $_SERVER['CASCADE_USER'] = 'cascadet_APIUSER';
+  $_SERVER['CASCADE_PASS'] = 'cascadet_APIUSERPASSWORDXU!@#$)*';
+  
+  
