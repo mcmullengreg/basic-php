87de0a6ebd522061f03c40e9480f2c00efac3f9f
Greg McMullen <mcmullen.greg@gmail.com>
2019-11-14 21:31:06 -0500

    Additional updates

--- api-tasks/v4-5-content-type.php
+++ api-tasks/v4-5-content-type.php
@@ -23,7 +23,43 @@
   $result = json_decode(curl_exec($ch), true);
   
   $pages = $result['asset']['folder']['children'];
-  
-  foreach ( $pages as $page ){
-    var_dump($page);
+
+  $multiCurl = array();
+  $result = array();
+  $mh = curl_multi_init();
+  // Loop each of the pages
+  foreach ( $pages as $i => $page ){
+    $id = $page['id'];
+    $type = $page['type'];
+    if ( $type == "page" ){
+      $fetchURL = $url . '/'.$type.'/' . $id;
+      $multiCurl[$i] = curl_init();
+      curl_setopt($multiCurl[$i], CURLOPT_URL, $fetchURL);
+      curl_setopt($multiCurl[$i], CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($multiCurl[$i], CURLOPT_POSTFIELDS, $auth);
+      curl_setopt($multiCurl[$i], CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($multiCurl[$i], CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($auth)
+      ));  
+      curl_multi_add_handle($mh, $multiCurl[$i]);
+    }
   }
+
+  $index = null;
+  do { 
+    curl_multi_exec($mh, $index);
+  } while ( $index > 0 );
+  
+  
+  foreach ( $multiCurl as $k => $ch){
+    $thisThing = json_decode(curl_multi_getcontent($ch), true);
+    $thisThing = $thisThing['asset']['page'];
+    $contentTypeID = $thisThing['contentTypeId'];
+    $metaData = $thisThing['metadata'];
+    $auth = $metaData['dynamicFields'][1];
+    $showNav = $metaData['dynamicFields'][0];
+    
+    curl_multi_remove_handle($mh, $ch);
+  }
+  curl_multi_close($mh);
